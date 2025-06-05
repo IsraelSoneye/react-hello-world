d6ddfa6b47f14d87cb79d59318d662442496832d
Israel Soneye <116042544+IsraelSoneye@users.noreply.github.com>
2025-06-05 16:06:27 +0100

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    
+    return ( 
+        <div className="home">
+            <h1>Hello World!</h1>
+            <p>Israel Soneye</p>
+        </div>
+     );
+}
+ 
+export default Home;
